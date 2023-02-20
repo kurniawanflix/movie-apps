ecca5e2ee6e1af4fec2a3e09ea250281ecb2b5f3
Muhammad David Kurniawan <kurniawanflix@gmail.com>
2023-02-20 09:29:16 +0700

    Initial Commit

---
+++ list-film-api/list_film_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  list_film_apiApp.swift
+//  list-film-api
+//
+//  Created by Muhammad David Kurniawan on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct list_film_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
