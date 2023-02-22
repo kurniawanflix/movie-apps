0d3363437835141c9183bf19ada3e8df42d2e8bc
Muhammad David Kurniawan <kurniawanflix@gmail.com>
2023-02-22 09:53:49 +0700

    Home Page UI - Not Fix

--- list-film-api/ContentView.swift
+++ list-film-api/ContentView.swift
@@ -9,13 +9,140 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            ScrollView(.vertical){
+                VStack{
+                    HStack{
+                        Text("Now Showing")
+                            .padding()
+                            .font(.custom("Merriweather-Black", size: 16))
+                        Spacer()
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            Text("See more")
+                                .padding()
+                                .font(.custom("Mulish-Light", size: 12))
+                                .foregroundColor(.secondary)
+                                .frame(height: 25)
+                                .background(RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.secondary))
+                                .padding()
+                        }
+                    }
+                    .padding(.top, 10)
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack(spacing: 20){
+                            VStack(alignment: .leading){
+                                Image("poster")
+                                    .resizable()
+                                    .frame(width: 143, height: 212)
+                                    .cornerRadius(8)
+                                    .shadow(radius: 10)
+                                Text("Spiderman: No Way Home")
+                                    .padding(4)
+                                    .font(.custom("Mulish-Medium", size: 14))
+                                    .lineLimit(1)
+                                HStack{
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(.yellow)
+                                    Text("9.1/10 IMDb")
+                                        .font(.custom("Mulish-Light", size: 12))
+                                }
+                                
+                            }
+                            .frame(width: 143, height: 240)
+                            VStack(alignment: .leading){
+                                Image("poster")
+                                    .resizable()
+                                    .frame(width: 143, height: 212)
+                                    .cornerRadius(8)
+                                    .shadow(radius: 10)
+                                Text("Spiderman: No Way Home")
+                                    .padding(4)
+                                    .font(.custom("Mulish-Medium", size: 14))
+                                HStack{
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(.yellow)
+                                    Text("9.1/10 IMDb")
+                                        .font(.custom("Mulish-Light", size: 12))
+                                }
+                            }
+                            .frame(width: 143, height: 240)
+                            VStack(alignment: .leading){
+                                Image("poster")
+                                    .resizable()
+                                    .frame(width: 143, height: 212)
+                                    .cornerRadius(8)
+                                    .shadow(radius: 10)
+                                Text("Spiderman: No Way Home")
+                                    .padding(4)
+                                    .font(.custom("Mulish-Medium", size: 14))
+                                HStack{
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(.yellow)
+                                    Text("9.1/10 IMDb")
+                                        .font(.custom("Mulish-Light", size: 12))
+                                }
+                            }
+                            .frame(width: 143, height: 240)
+                        }
+                        .padding()
+                        .padding(.bottom, 20)
+                    }
+                    HStack{
+                        Text("Popular")
+                            .padding()
+                            .font(.custom("Merriweather-Black", size: 16))
+                        Spacer()
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            Text("See more")
+                                .padding()
+                                .font(.custom("Mulish-Light", size: 12))
+                                .foregroundColor(.secondary)
+                                .frame(height: 25)
+                                .background(RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.secondary))
+                                .padding()
+                        }
+                    }
+                    VStack(alignment: .leading){
+                        HStack{
+                            Image("poster")
+                                .resizable()
+                                .frame(width: 143, height: 212)
+                                .cornerRadius(8)
+                                .shadow(radius: 10)
+                            VStack(alignment: .leading, spacing: 10){
+                                Text("Spiderman: No Way Home")
+                                HStack{
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(.yellow)
+                                    Text("9.1/10 IMDb")
+                                }
+                            }
+                        }
+                    }
+                    
+                    Spacer()
+                }
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar{
+                    ToolbarItem(placement: .principal){
+                        HStack{
+                            Image(systemName: "line.3.horizontal.decrease")
+                            Spacer()
+                            Text("Filmku")
+                                .font(.custom("Merriweather-Black", size: 16))
+                            Spacer()
+                            Image(systemName: "bell.badge")
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
